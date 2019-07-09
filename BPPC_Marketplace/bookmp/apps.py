2f820bde38ada1c46c6f9a6a9ca433af34a7b428
theweblover007 <theweblover007@gmail.com>
2019-07-09 10:23:03 +0530

    Added the main folder and requirements.txt

---
+++ BPPC_Marketplace/bookmp/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class BookmpConfig(AppConfig):
+    name = 'bookmp'
---
+++ BPPC_Marketplace/bookmp/models.py
@@ -0,0 +1,41 @@
+from django.db import models
+from django.contrib.auth.models import User
+
+class Profile(models.Model):
+	YEAR_CHOICES = (
+		('1','1st Year'),
+		('2','2nd Year'),
+		('3','3rd Year'),
+		('4','4th Year'),
+		('5','5th Year'),
+	)
+	
+	SINGLE_DEGREE_BRANCH_CHOICES = (
+		('CHEMENGG','B.E. Chemical'),
+		('CIVILENGG','B.E. Civil'),
+		('CS','B.E. Computer Science'),
+		('EEE','B.E. Electrical & Electronics'),
+		('ENI','B.E. Electronics & Instrumentation'),
+		('MECHENGG','B.E. Mechanical'),
+		('MANUENGG','B.E. Manufacturing'),
+		('BPHARMA','B.Pharm.'),
+	)
+
+	DUAL_DEGREE_BRANCH_CHOICES = (
+		('BIO','M.Sc. Biological Sciences'),
+		('CHEM','M.Sc. Chemistry'),
+		('ECO','M.Sc. Economics'),
+		('MATHS','M.Sc. Mathematics'),
+		('PHY','M.Sc. Physics'),
+	)
+
+	user = models.OneToOneField(User,on_delete=models.CASCADE,related_name='profile')
+	bits_id = models.CharField(max_length=13,null=False)
+	year=models.IntegerField(choices=YEAR_CHOICES)
+	is_dual_degree = models.BooleanField(default=False)
+	single_branch = models.CharField(max_length=100,choices=SINGLE_DEGREE_BRANCH_CHOICES)
+	dual_branch= models.CharField(max_length=100,choices=DUAL_DEGREE_BRANCH_CHOICES,null=True)
+
+	def __str__(self):
+		return 'Profile for user {}'.format(self.user.username)
+
