93874c155b9314f52c447094e866f3892176d685
theweblover007 <theweblover007@gmail.com>
2019-07-17 16:56:12 +0530

    Fixed a small error in urls.py by removing the leading /

--- BPPC_Marketplace/bookmp/urls.py
+++ BPPC_Marketplace/bookmp/urls.py
@@ -3,7 +3,7 @@ from django.urls import include, path
 
 
 urlpatterns=[
-    path('/auth/login/', views.login, name='login'),   
-    path('/auth/signup/', views.signup, name='signup'),   
+    path('auth/login/', views.login, name='login'),   
+    path('auth/signup/', views.signup, name='signup'),   
 
 ]    
