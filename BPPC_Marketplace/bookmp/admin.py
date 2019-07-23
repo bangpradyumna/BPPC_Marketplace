64ef18962e6e686825862cf305065cb363b7eacb
theweblover007 <theweblover007@gmail.com>
2019-07-23 17:53:09 +0530

    Registered models on admin.py

--- BPPC_Marketplace/bookmp/admin.py
+++ BPPC_Marketplace/bookmp/admin.py
@@ -1,7 +1,12 @@
 from django.contrib import admin
 
-from .models import Profile
+from .models import Profile, BookInstance, BookClass, Course, Image, Seller
 
 
 # Register your models here.
 admin.site.register(Profile)
+admin.site.register(BookInstance)
+admin.site.register(BookClass)
+admin.site.register(Course)
+admin.site.register(Image)
+admin.site.register(Seller)
