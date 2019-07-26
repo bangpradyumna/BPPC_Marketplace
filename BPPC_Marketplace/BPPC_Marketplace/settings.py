9b5f604c63cca14c43adfe4105c2ec56cf7b5d44
theweblover007 <theweblover007@gmail.com>
2019-07-26 23:25:35 +0530

    Changed DEFAULT_PERMISSION_CLASS from AllowAny to IsAuthenticated

--- BPPC_Marketplace/BPPC_Marketplace/settings.py
+++ BPPC_Marketplace/BPPC_Marketplace/settings.py
@@ -110,11 +110,11 @@ else:
 # http://getblimp.github.io/django-rest-framework-jwt/
 REST_FRAMEWORK = {
     'DEFAULT_PERMISSION_CLASSES': (
-        'rest_framework.permissions.AllowAny',
+        'rest_framework.permissions.IsAuthenticated',
     ),
-    'DEFAULT_RENDERER_CLASSES': (
-        'rest_framework.renderers.JSONRenderer',
-    ),
+    # 'DEFAULT_RENDERER_CLASSES': (
+    #     'rest_framework.renderers.JSONRenderer',
+    # ),
     'DEFAULT_AUTHENTICATION_CLASSES': (
         'rest_framework_jwt.authentication.JSONWebTokenAuthentication',
         'rest_framework.authentication.SessionAuthentication',
