b00734b10eae84fcf8eefba28638fc05860339f1
Shivanshu Ayachi <shivanshuayachi@gmail.com>
2019-07-29 19:43:06 +0530

    Fix function for auto-deletion of images

--- BPPC_Marketplace/bookmp/models.py
+++ BPPC_Marketplace/bookmp/models.py
@@ -175,6 +175,6 @@ def auto_delete_file_on_delete(sender, instance, **kwargs):
     Deletes file from system
     when corresponding `Image` object is deleted.
     """
-    if instance.file:
-        if os.path.isfile(instance.file.path):
-            os.remove(instance.file.path)
+    if instance.img:
+        if os.path.isfile(instance.img.path):
+            os.remove(instance.img.path)
