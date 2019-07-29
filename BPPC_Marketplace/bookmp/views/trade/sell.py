f2c91ba41acb242a1978ac3360eba17cfcfad6e5
Shivanshu Ayachi <shivanshuayachi@gmail.com>
2019-07-29 20:36:06 +0530

    Add MultiPartParser to sel view for image uploads

--- BPPC_Marketplace/bookmp/views/trade/sell.py
+++ BPPC_Marketplace/bookmp/views/trade/sell.py
@@ -8,8 +8,9 @@ from django.http import HttpResponse
 from django.views.decorators.csrf import csrf_exempt
 from google.auth.transport import requests as google_requests
 from google.oauth2 import id_token as googleIdToken
-from rest_framework.decorators import api_view, permission_classes
+from rest_framework.decorators import api_view, permission_classes, parser_classes
 from rest_framework.permissions import IsAuthenticated
+from rest_framework.parsers import MultiPartParser
 from rest_framework.response import Response
 
 from bookmp.models import BookClass, BookInstance, Course, Image, Profile, Seller
@@ -24,6 +25,7 @@ DOMAIN_NAME = 'https://market.bits-dvm.org'
 @csrf_exempt
 @api_view(['POST', 'GET'])
 @permission_classes((IsAuthenticated,))
+@parser_classes([MultiPartParser]) # For image uploads.
 @transaction.atomic
 def sell(request):
 
